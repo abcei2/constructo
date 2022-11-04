aa472f710f2d9bc80758bdd49808bb9496410a6f
abcei2 <santiago.salgado.duque@gmail.com>
2022-11-04 12:23:57 -0500

    forgot types

--- types/extraTypes.tsx
+++ types/extraTypes.tsx
@@ -1,6 +1,13 @@
-import { EmployeeWage } from "./dbTypes"
+import { EmployeeWage, Product } from "./dbTypes"
 
 
 export type WagesFormTypes = {
     employeesWage: Array<EmployeeWage>
+}
+
+export type ProductsFormType = {
+    concept: string,
+    updateDate: Date;
+    owner: string;
+    products: Array<Product>
 }
