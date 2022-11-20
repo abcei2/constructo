187491f3177befdc7d6728593f8a086874a1e750
abcei2 <santiago.salgado.duque@gmail.com>
2022-11-20 17:26:43 -0500

    Updating project lists styles

--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -22,42 +22,48 @@ const ProjectsList = () => {
     return user ? (
         <div className="">
             <div className="flex fixed w-full justify-end ">
+              
                 <div>
 
                     <button className="button-secondary  m-10 " 
                         onClick={() => router.replace({pathname: '/create'})}>Crear proyecto</button>
                 </div>
             </div>
-            <div className="overflow-auto  ">
+            <div className="  my-10 items-center flex  ">
+                <div className="text-center w-full md:text-4xl text-3xl ">
+                    Lista de proyectos
+                </div>
+            </div>
+            <div className="flex flex-col h-full place-content-center">
+                    {
+                        projects.length > 0 ? projects.map(
+                            (project: Project | any, index: number) => {
+                                return <div key={index} onClick={() => router.replace({
+                                    pathname: '/management',
+                                    query: { projectRef: project.ref }
+                                })} className="hover:from-blue-300 hover:to-teal-300 bg-gradient-to-r p-10  lg:flex lg:justify-center">
+                                    <div className=" w-full lg:max-w-[70%] lg:flex ">
+                                        <div className="hover:from-teal-500 hover:to-blue-500 bg-gradient-to-r from-blue-500 to-teal-500 h-24 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" >
 
-                {
-                    projects.length>0 ? projects.map(
-                        (project: Project | any, index: number) => {
-                            return <div key={index} onClick={() => router.replace({
-                                pathname: '/management',
-                                query: { projectRef: project.ref }
-                            })} className="hover:from-blue-300 hover:to-teal-300 bg-gradient-to-r p-10 lg:flex lg:justify-center">
-                                <div className=" w-full lg:max-w-[80%] lg:flex ">
-                                    <div className="hover:from-teal-500 hover:to-blue-500 bg-gradient-to-r from-blue-500 to-teal-500 h-24 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" >
+                                        </div>
+                                        <div className="text-center w-full border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                                            <div className="m-4 ">
 
-                                    </div>
-                                    <div className="text-center w-full border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                                        <div className="m-4 ">
+                                                <div className="font-bold text-xl mb-2">NOMBRE DEL PROYECTO:</div>
+                                                <div className=" text-xl mb-2">{project.name}</div>
 
-                                            <div className="font-bold text-xl mb-2">NOMBRE DEL PROYECTO:</div>
-                                            <div className=" text-xl mb-2">{project.name}</div>
-
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        }
-                    ) : <div className="  h-screen items-center flex  ">
+                            }
+                        ) : <div className="  h-screen items-center flex  ">
                             <div className="text-center w-full md:text-4xl text-3xl">
                                 Aún no has creado un proyecto
                             </div>
-                    </div>
-                }
+                        </div>
+                    }                 
+             
             </div>
           
         </div>
