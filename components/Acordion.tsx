556aabecc4d0b32c9fb7ab403f0bd54a65f301c9
abcei2 <santiago.salgado.duque@gmail.com>
2022-11-23 04:59:15 -0500

    just styling

--- components/Acordion.tsx
+++ components/Acordion.tsx
@@ -3,10 +3,19 @@ const Acordion = (props: {
     children: React.ReactNode;
 }) => {
     const {headerText, children} = props
-    return <div className=" space-y-2 border rounded border-gray-400  ">
+    return <div className="bg-white  border rounded border-gray-400 ">
             <details className="rounded-lg">
-            <summary className="font-semibold">{headerText}</summary>
-                {children}
+                <summary className="font-semibold flex justify-between p-3">
+                    <button className="button-secondary">Save</button>
+                    <div>
+                        {headerText}
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7"></path></svg>    
+                    </div>
+                    
+                </summary>
+                <div className="border-t border-gray-300">
+                    {children}
+                </div>
             </details>
         </div>
 }
--- components/project/stages/index.tsx
+++ components/project/stages/index.tsx
@@ -3,52 +3,54 @@ import { Stage } from "../../../types/dbTypes";
 import Acordion from "../../Acordion";
 import StageCategories from "./StageCategories";
 
-const Stages = (props:{
-    projectRef:string
-}) =>{
+const Stages = (props: {
+    projectRef: string
+}) => {
 
     const { projectRef } = props
     const [newStageDescription, setNewStageDescription] = useState<string>("")
-    const [ stageList, setStageList ] = useState<Array<Stage>>([])
+    const [stageList, setStageList] = useState<Array<Stage>>([])
 
-    const addStage = () =>{
+    const addStage = () => {
         setStageList((oldStageList) => {
             return [
                 ...oldStageList,
-                {description:newStageDescription}
+                { description: newStageDescription }
             ]
         })
     }
+
+    return projectRef ? (<div className=" max-h-screen">
     
-    return projectRef ?(
-        <div className=" flex flex-col lg:px-20 p-5 gap-4 h-screen place-content-center overflow-scroll ">
-
-            <div className="h-32 w-full flex my-5 gap-5">
-                <textarea className="w-[100%] border rounded border-gray-400 px-3 " onChange={
-                    (e: ChangeEvent<HTMLTextAreaElement>) => {
-                        setNewStageDescription(e.currentTarget.value)
-                    }
-                }/>
-                <button onClick={addStage}
+        <div className=" w-full flex p-5 gap-5">
+            <textarea className="w-[100%] border rounded border-gray-400 px-3 " onChange={
+                (e: ChangeEvent<HTMLTextAreaElement>) => {
+                    setNewStageDescription(e.currentTarget.value)
+                }
+            } />
+            <button onClick={addStage}
                 className="button-secondary">
-                    Agrergar categoría
-                </button>
-            </div>
+                Agrergar etapa
+            </button>
+        </div>
 
+        <div className="flex flex-col lg:px-20 p-5 gap-4 max-h-full  overflow-auto ">
 
-            <div className="h-screen ">
+            <div className="h-screen flex flex-col   gap-4   ">
                 {
                     stageList.map(
                         (stageItem, index) => <Acordion key={index} headerText={stageItem.description}>
-                                <StageCategories projectRef={projectRef} />
-                            </Acordion>
+                            <StageCategories projectRef={projectRef} />
+                        </Acordion>
                     )
                 }
 
             </div>
 
-        </div>    
-    ):<></>
+        </div>
+  
+    </div>
+    ) : <></>
 }
 
 export default Stages
